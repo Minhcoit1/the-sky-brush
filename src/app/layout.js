b5b63f4f36a2a7953100ef1cb45edef8eaf1016f
Bui Ngoc Minh <Minhbuibk86@gmail.com>
2025-08-14 07:14:44 +0700

    update base code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,36 +1,33 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
 import Navbar from "@/components/navbar/Navbar";
-import "./globals.css";
-import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
-import AuthProvider from "@/providers/AuthProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Lama Dev Blog App",
-  description: "The best blog app!",
-};
+  title: 'The Sky Touch',
+  description: 'Đây là blog của Minh Bùi',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <ThemeContextProvider>
-            <ThemeProvider>
-              <div className="container">
-                <div className="wrapper">
-                  <Navbar />
-                  {children}
-                  <Footer />
-                </div>
+        <ThemeContextProvider>
+          <ThemeProvider>
+            <div className='container'>
+              <div className='wrapper'>
+                <Navbar />
+                {children}
+                <Footer />
               </div>
-            </ThemeProvider>
-          </ThemeContextProvider>
-        </AuthProvider>
+            </div>
+          </ThemeProvider>
+        </ThemeContextProvider>
       </body>
     </html>
-  );
+  )
 }
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,30 +1,10 @@
-"use client";
+import React from "react"
+import styles from "./pagination.module.css"
 
-import React from "react";
-import styles from "./pagination.module.css";
-import { useRouter } from "next/navigation";
-
-const Pagination = ({ page, hasPrev, hasNext }) => {
-  const router = useRouter();
-
-  return (
-    <div className={styles.container}>
-      <button
-        className={styles.button}
-        disabled={!hasPrev}
-        onClick={() => router.push(`?page=${page - 1}`)}
-      >
-        Previous
-      </button>
-      <button
-        disabled={!hasNext}
-        className={styles.button}
-        onClick={() => router.push(`?page=${page + 1}`)}
-      >
-        Next
-      </button>
-    </div>
-  );
-};
+const Pagination = () => {
+    return (
+        <div className={styles.container}>Pagination</div>
+    )
+}
 
 export default Pagination;
